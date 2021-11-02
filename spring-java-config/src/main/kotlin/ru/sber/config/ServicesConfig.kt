b03e34dc06580580e668b946b61e396eeaf0effd
Stanislav Naumenko <Naumenko.s.vik@sberbank.ru>
2021-11-02 13:21:51 +0300

    ДЗ

--- spring-java-config/src/main/kotlin/ru/sber/config/ServicesConfig.kt
+++ spring-java-config/src/main/kotlin/ru/sber/config/ServicesConfig.kt
@@ -3,21 +3,18 @@ package ru.sber.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import ru.sber.services.FirstService
+import ru.sber.services.FourthService
 
 @Configuration
-@ComponentScan
-class ServicesConfig {
-    @Bean
-    fun service(): FirstService {
-        return FirstService()
-    }
+@ComponentScan("ru.sber.services")
+class ServicesConfig {}
+
+@Configuration
+class AnotherServicesConfig {
 
     @Bean
-    fun secondService() {
+    fun fourthService(): FourthService {
+        return FourthService()
     }
+
 }
-
-@Configuration
-@ComponentScan("ru.sber.anotherservices")
-class AnotherServicesConfig
--- spring-java-config/src/main/kotlin/ru/sber/services/Services.kt
+++ spring-java-config/src/main/kotlin/ru/sber/services/Services.kt
@@ -1,21 +1,22 @@
 package ru.sber.services
 
-import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 
+@Service
 class FirstService {
     override fun toString(): String {
         return "I am firstService"
     }
 }
 
+@Service
 class SecondService {
     override fun toString(): String {
         return "I am secondService"
     }
 }
 
-@Component
+@Service
 class ThirdService {
     override fun toString(): String {
         return "I am thirdService"
